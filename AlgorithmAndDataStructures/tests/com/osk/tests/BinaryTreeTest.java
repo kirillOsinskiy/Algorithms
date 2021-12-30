4019d5b3d20ab12f93ae5f0db1a2d23cc2236933
kirill.osinskiy@gmail.com <M4trix_Ha$_Y0u>
2021-12-30 15:03:34 +0300

    add gradle

--- AlgorithmAndDataStructures/tests/com/osk/tests/BinaryTreeTest.java
+++ AlgorithmAndDataStructures/tests/com/osk/tests/BinaryTreeTest.java
@@ -2,12 +2,9 @@ package com.osk.tests;
 
 import com.osk.trees.BinaryTree;
 import com.osk.trees.Node;
-import org.junit.Before;
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BinaryTreeTest {
 
